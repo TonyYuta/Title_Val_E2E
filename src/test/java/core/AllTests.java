8f93200f608331827a6677f80788ffb3d73291d0
TonyYuta <tonykagaya@gmail.com>
2016-04-10 11:48:23 -0700

    multiple title validation. JUnit framework

---
+++ src/test/java/core/AllTests.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: AllTests.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Jan 20, 2016
+ *   
+ */
+
+package core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * AllTests 
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ SeleniumTest.class })
+public class AllTests {
+
+}
